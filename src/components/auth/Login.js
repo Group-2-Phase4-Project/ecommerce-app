a1ce4b6d39ad2ead172d9fa9ba32f74bafacf20f
Diana <maritimdiane96@gmail.com>
2023-03-29 22:33:05 +0300

    Login form

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,14 +1,107 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Form, Button, Container, Row, Col, Modal } from 'react-bootstrap';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Signup from './Signup';
 
 
 
 
-function Login (){
-    return(
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
-}
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showResetModal, setShowResetModal] = useState(false);
+  const [resetEmail, setResetEmail] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/api/login', {
+        email,
+        password,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleReset = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/api/reset', {
+        email: resetEmail,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Container>
+      <Row className="justify-content-md-center mt-5">
+        <Col xs={12} md={6}>
+          <h1 className="text-center mb-4">Log In</h1>
+          <Form onSubmit={handleLogin}>
+            <Form.Group controlId="formEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Log In
+            </Button>
+            <Button
+              variant="link"
+              onClick={() => setShowResetModal(true)}
+              className="float-right"
+            >
+              Forgot Password?
+            </Button>
+            <div className="mt-3">
+              Don't have an account? <Link exact="/Signup">Sign up here</Link>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+      <Modal show={showResetModal} onHide={() => setShowResetModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Reset Password</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleReset}>
+            <Form.Group controlId="formResetEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={resetEmail}
+                onChange={(e) => setResetEmail(e.target.value)}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Reset Password
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </Container>
+  );
+};
 
 export default Login;
